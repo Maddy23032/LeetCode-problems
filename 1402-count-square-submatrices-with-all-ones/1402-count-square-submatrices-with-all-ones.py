b2318536906867aeac8ad20416cb758f7d341484
Maddy23032 <145091861+Maddy23032@users.noreply.github.com>
2024-10-27 17:22:25 +0530

    Time: 73 ms (39.13%), Space: 19.3 MB (20.45%) - LeetHub

---
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countSquares(self, matrix: List[List[int]]) -> int:
+        ROWS, COLS = len (matrix), len(matrix[0])
+        dp = defaultdict(int)
+        res = 0
+        for r in range (ROWS):
+            cur_dp = defaultdict(int)
+            for c in range(COLS):
+                if matrix[r][c]:
+                    cur_dp[c] = 1 + min(
+                        dp[c],
+                        cur_dp[c - 1],
+                        dp[c - 1]
+                    )
+                    res += cur_dp[c]
+            dp = cur_dp
+        return res
