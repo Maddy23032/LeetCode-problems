de4d7f903aca33bb63749e22af1d7a7a15f36625
Madhu_Karthick <145091861+Maddy23032@users.noreply.github.com>
2025-08-01 06:04:14 +0530

    Time: 0 ms (100%), Space: 18.5 MB (37.05%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.py
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.py
@@ -1,22 +1,28 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        def check(st,color,n,graph):
-            q=deque([st])
-            color[st]=0
-            while q:
-                node=q.popleft()
-                for i in graph[node]:
-                    if color[i]==-1:
-                        color[i]=0 if color[node]==1 else 1
-                        q.append(i)
-                    elif color[node]==color[i]:
+        def dfs(st,col,color,graph):
+            # q=deque([st])
+            # color[st]=0
+            # while q:
+            #     node=q.popleft()
+            #     for i in graph[node]:
+            #         if color[i]==-1:
+            #             color[i]=0 if color[node]==1 else 1
+            #             q.append(i)
+            #         elif color[node]==color[i]:
+            #             return False
+            # return True
+            color[st]=col
+            for i in graph[st]:
+                if color[i]==-1:
+                    if (not dfs(i,0 if col==1 else 1,color,graph)):
                         return False
+                elif color[i]==col:return False
             return True
         n=len(graph)
         color=[-1 for _ in range(n)]
-
         for i in range(n):
             if color[i]==-1:
-                if check(i,color,n,graph)==False:
+                if dfs(i,0,color,graph)==False:
                     return False
         return True
