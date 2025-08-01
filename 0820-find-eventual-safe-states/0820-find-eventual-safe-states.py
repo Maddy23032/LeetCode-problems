164032aaf21396379b28e27a199582f6c06aeb36
Madhu_Karthick <145091861+Maddy23032@users.noreply.github.com>
2025-08-01 21:17:42 +0530

    Time: 51 ms (51.95%), Space: 23.5 MB (76.35%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.py
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.py
@@ -1,17 +1,34 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        safe={}
-        def dfs(i):
-            if i in safe:
-                return safe[i]
-            safe[i]=False
+        n=len(graph)
+        indeg=[0 for _ in range(n)]
+        adj=[[] for _ in range(n)]
+
+        for i in range(n):
             for j in graph[i]:
-                if not dfs(j):
-                    return safe[i]
-            safe[i]=True
-            return safe[i]
+                adj[j].append(i)
+                indeg[i]+=1
+        q=deque()
+        for i in range(n):
+            if not indeg[i]:
+                q.append(i)
         res=[]
-        for i in range(len(graph)):
-            if dfs(i):
-                res.append(i)
-        return res
+        while q:
+            node=q.popleft()
+            res.append(node)
+            for i in adj[node]:
+                indeg[i]-=1
+                if not indeg[i]:
+                    q.append(i)
+        
+        return sorted(res)
+        
+        # for i in range(n):
+        #     outdeg[i]=len(graph[i])
+        
+        # q=deque()
+        # for i in range(n):
+        #     if not outdeg[i]:
+        #         q.append(i)
+        # print(outdeg)
+        # return []
