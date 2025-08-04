a34c4750a03817c11b893e0e5345ece8f6561978
MADHU KARTHICK A <145091861+Maddy23032@users.noreply.github.com>
2025-08-04 05:53:45 +0530

    Time: 3 ms (91.97%), Space: 19.3 MB (48.86%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.py
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.py
@@ -1,26 +1,20 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        lis=defaultdict(list)
+        adj=[[] for _ in range(n)]
         for i in flights:
-            lis[i[0]].append([i[1],i[2]])
-        prices=[float('inf') for _ in range(n)]
-        prices[src]=0
-        q=deque()
-        q.append([0,src,0])
+            adj[i[0]].append([i[1],i[2]])
+        q=deque([[0,src,0]])
+        dist=[float('inf') for _ in range(n)]
+        dist[src]=0
+
         while q:
-            curr=q.popleft()
-            print(curr)
-            cost=curr[0]
-            city=curr[1]
-            stops=curr[2]
-
-            if stops>k:continue
-
-            for nei in lis[city]:
-                neicity=nei[0]
-                newcost=nei[1]+cost
-                if newcost<prices[neicity]:
-                    prices[neicity]=newcost
-                    q.append([prices[neicity],neicity,stops+1])
-            
-        return -1 if prices[dst]==float('inf') else prices[dst]
+            stops,node,cost=q.popleft()
+            if stops>k:
+                continue
+            for i in adj[node]:
+                adjnode=i[0]
+                edw=i[1]
+                if cost+edw<dist[adjnode] and stops<=k:
+                    dist[adjnode]=cost+edw
+                    q.append([stops+1,adjnode,cost+edw])
+        return -1 if dist[dst]==float('inf') else dist[dst]
