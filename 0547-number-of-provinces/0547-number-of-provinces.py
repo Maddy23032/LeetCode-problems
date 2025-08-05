7996725f289752703022d00bde3e0b1c0afd2afd
MADHU KARTHICK A <145091861+Maddy23032@users.noreply.github.com>
2025-08-05 20:49:51 +0530

    Time: 6 ms (60.12%), Space: 18.7 MB (99.4%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,14 +1,41 @@
+from typing import List
+
+class DisjointSet:
+    def __init__(self, n: int):
+        self.parent = [i for i in range(n)]
+        self.rank = [0] * n
+        self.size = [1] * n
+
+    def findParent(self, node: int) -> int:
+        if self.parent[node] != node:
+            self.parent[node] = self.findParent(self.parent[node])
+        return self.parent[node]
+
+    def unionBySize(self, u: int, v: int) -> None:
+        pu = self.findParent(u)
+        pv = self.findParent(v)
+        if pu == pv:
+            return
+        if self.size[pu] < self.size[pv]:
+            self.parent[pu] = pv
+            self.size[pv] += self.size[pu]
+        else:
+            self.parent[pv] = pu
+            self.size[pu] += self.size[pv]
+
+
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        def dfs(st):
-            vis[st]=1
-            for i in range(len(isConnected[st])):
-                if not vis[i] and isConnected[st][i]:
-                    dfs(i)
-        vis=[0]*len(isConnected)
-        number=0
-        for i in range(len(isConnected)):
-            if not vis[i]:
-                number+=1
-                dfs(i)
-        return number
+        n = len(isConnected)
+        dis = DisjointSet(n)
+
+        for i in range(n):
+            for j in range(i+1, n):  # check only upper triangle
+                if isConnected[i][j] == 1:
+                    dis.unionBySize(i, j)
+
+        provinces = set()
+        for i in range(n):
+            provinces.add(dis.findParent(i))  # collect unique parents
+        
+        return len(provinces)
