501ab8cb617a932000b7ad1491f94a2c260f9ae4
MADHU KARTHICK A <145091861+Maddy23032@users.noreply.github.com>
2025-08-11 06:09:51 +0530

    Time: 0 ms (100%), Space: 17.9 MB (45.86%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,17 +1,27 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        def uniqP(a,b,dp):
-            nonlocal m,n
-            if a==m-1 and b==n-1:
-                dp[a][b]=1
-                return dp[a][b]
-            if a >= m or b >= n:
-                return 0
-            if dp[a][b]!=-1:
-                return dp[a][b]
-            dp[a][b]=uniqP(a+1,b,dp)+uniqP(a,b+1,dp)
-            return dp[a][b]
+        # def uniqP(a,b,dp):
+        #     nonlocal m,n
+        #     if a==m-1 and b==n-1:
+        #         dp[a][b]=1
+        #         return dp[a][b]
+        #     if a >= m or b >= n:
+        #         return 0
+        #     if dp[a][b]!=-1:
+        #         return dp[a][b]
+        #     dp[a][b]=uniqP(a+1,b,dp)+uniqP(a,b+1,dp)
+        #     return dp[a][b]
 
         
-        dp=[[-1 for _ in range(n)] for _ in range(m)]
-        return uniqP(0,0,dp)
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        dp[m-1][n-1] = 1  # base case
+        
+        for i in range(m-1, -1, -1):
+            for j in range(n-1, -1, -1):
+                if i == m-1 and j == n-1:
+                    continue
+                down = dp[i+1][j] if i+1 < m else 0
+                right = dp[i][j+1] if j+1 < n else 0
+                dp[i][j] = down + right
+        
+        return dp[0][0]
