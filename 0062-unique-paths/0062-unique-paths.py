acaea0551a2ae17c90dc7bc53cd75b82ce171580
MADHU KARTHICK A <145091861+Maddy23032@users.noreply.github.com>
2025-08-11 05:35:15 +0530

    Time: 0 ms (100%), Space: 17.9 MB (45.86%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,25 +1,17 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        # l=[[0]*n for _ in range(m)]
-        # for i in range(m):
-        #     l[i][n-1]=1
-        # for i in range(n):
-        #     l[m-1][i]=1
-        # for i in range(m-2,-1,-1):
-        #     for j in range(n-2,-1,-1):
-        #         l[i][j]=l[i+1][j]+l[i][j+1]
-        # return l[0][0]
-        def upr(x,y,m,n,l):
-            if x==m-1 and y==n-1:
-                return 1
-            if l[x][y]!=-1:
-                return l[x][y]
-            rp,dp=0,0
-            if x<m-1:
-                rp=upr(x+1,y,m,n,l)
-            if y<n-1:
-                dp=upr(x,y+1,m,n,l)
-            l[x][y]=rp+dp
-            return l[x][y]
-        l=[[-1]*n for _ in range(m)]
-        return upr(0,0,m,n,l)
+        def uniqP(a,b,dp):
+            nonlocal m,n
+            if a==m-1 and b==n-1:
+                dp[a][b]=1
+                return dp[a][b]
+            if a >= m or b >= n:
+                return 0
+            if dp[a][b]!=-1:
+                return dp[a][b]
+            dp[a][b]=uniqP(a+1,b,dp)+uniqP(a,b+1,dp)
+            return dp[a][b]
+
+        
+        dp=[[-1 for _ in range(n)] for _ in range(m)]
+        return uniqP(0,0,dp)
