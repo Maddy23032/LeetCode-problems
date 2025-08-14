1a5d9d08bbffc1452f6491349aaa8a91ff2a5f66
MADHU KARTHICK A <145091861+Maddy23032@users.noreply.github.com>
2025-08-14 14:44:45 +0530

    Time: 17 ms (5.48%), Space: 21 MB (11.62%) - LeetHub

---
+++ 0120-triangle/0120-triangle.py
@@ -0,0 +1,30 @@
+# class Solution:
+#     def minimumTotal(self, triangle: List[List[int]]) -> int:
+#         n=len(triangle)
+#         def uniqP(a,b,triangle,dp):
+#             nonlocal n
+#             if a==n-1:
+#                 dp[a][b]=triangle[a][b]
+#                 return dp[a][b]
+#             if dp[a][b]!=-1:
+#                 return dp[a][b]
+#             d=triangle[a][b]+uniqP(a+1,b,triangle,dp)
+#             dq=triangle[a][b]+uniqP(a+1,b+1,triangle,dp)
+#             dp[a][b]=min(d,dq)
+#             return dp[a][b]
+        
+#         dp=[[-1 for _ in range(n)] for _ in range(n)]
+#         return uniqP(0,0,triangle,dp)
+
+def recursion(l,x,y,ans,memo):
+    if x>=len(l):
+        return 0
+    if (x,y) in memo:
+        return memo[(x,y)]
+    ans += l[x][y]+min(recursion(l,x+1,y,ans,memo),recursion(l,x+1,y+1,ans,memo))
+    memo[(x, y)] = ans
+    return ans
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        return recursion(triangle,0,0,0,{})
+        
